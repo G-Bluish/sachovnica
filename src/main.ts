4d04ce8bfe8dbd54a044ea7b8f2d6762838e64ed
G-Bluish <gabik.brezina@gmail.com>
2026-09-19 12:40:30 +0200

    pridanie popisiek

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,69 @@
 const sachovnica = document.getElementById("sachovnica");
 if (sachovnica === null) {
-   throw new Error("Kontajner sachovnice neexistuje.");
+    throw new Error("Kontajner sachovnice neexistuje.");
 }
+const lhRoh = document.createElement("div");
+lhRoh.classList.add("roh");
+lhRoh.classList.add("horna");
+lhRoh.classList.add("lavy");
+sachovnica.append(lhRoh);
+
+for (let stlpec = 0; stlpec < 8; stlpec++) {
+    const hPopiska = document.createElement("div");
+    hPopiska.classList.add("popiska");
+    hPopiska.classList.add("horna");
+    hPopiska.textContent = String.fromCharCode(65 + stlpec);
+    sachovnica.append(hPopiska);
+}
+
+const phRoh = document.createElement("div");
+phRoh.classList.add("roh");
+phRoh.classList.add("horna");
+phRoh.classList.add("prava");
+sachovnica.append(phRoh);
+
 for (let riadok = 0; riadok < 8; riadok++) {
-   for (let stlpec = 0; stlpec < 8; stlpec++) {
-       const policko = document.createElement("div");
-       policko.classList.add("policko");
-       if ((riadok + stlpec) % 2 === 0) {
-           policko.classList.add("svetle");
-       } else {
-           policko.classList.add("tmave");
-       }
-       sachovnica.append(policko);
-   }
+    const lPopiska = document.createElement("div");
+    lPopiska.classList.add("popiska");
+    lPopiska.classList.add("lava");
+    lPopiska.textContent = `${8 - riadok}`;
+    sachovnica.append(lPopiska);
+
+    for (let stlpec = 0; stlpec < 8; stlpec++) {
+        const policko = document.createElement("div"); // Vytvorenie elementu div pre policko
+        policko.classList.add("policko"); // Pridanie triedy "policko" pre styling
+        if ((riadok + stlpec) % 2 === 0) {
+            policko.classList.add("svetle"); // Pridanie triedy "svetle" pre svetlé policko
+        } else {
+            policko.classList.add("tmave");
+        }
+        //policko.textContent = String.fromCharCode(97 + stlpec) + `${8 - riadok}`;
+        sachovnica.append(policko); // Pridanie policka do kontajnera sachovnice
+    }
+
+    const pPopiska = document.createElement("div");
+    pPopiska.classList.add("popiska");
+    pPopiska.classList.add("prava");
+    sachovnica.append(pPopiska);
+    pPopiska.textContent = `${8 - riadok}`;
 }
+
+const ldRoh = document.createElement("div");
+ldRoh.classList.add("roh");
+ldRoh.classList.add("dolna");
+ldRoh.classList.add("lavy");
+sachovnica.append(ldRoh); // Pridanie ľavého dolného rohu
+
+for (let stlpec = 0; stlpec < 8; stlpec++) {
+    const dPopiska = document.createElement("div");
+    dPopiska.classList.add("popiska");
+    dPopiska.classList.add("dolna");
+    dPopiska.textContent = String.fromCharCode(97 + stlpec);
+    sachovnica.append(dPopiska);
+}
+
+const pdRoh = document.createElement("div");
+pdRoh.classList.add("roh");
+pdRoh.classList.add("dolna");
+pdRoh.classList.add("prava");
+sachovnica.append(pdRoh);
