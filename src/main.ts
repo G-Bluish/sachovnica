dafe759890232ef51c0bf53f21010f0c9f6a3a30
G-Bluish <gabik.brezina@gmail.com>
2026-09-19 15:06:29 +0200

    dvojty okraj sachovnice

--- src/main.ts
+++ src/main.ts
@@ -2,68 +2,55 @@ const sachovnica = document.getElementById("sachovnica");
 if (sachovnica === null) {
     throw new Error("Kontajner sachovnice neexistuje.");
 }
-const lhRoh = document.createElement("div");
-lhRoh.classList.add("roh");
-lhRoh.classList.add("horna");
-lhRoh.classList.add("lavy");
+
+function vytvorTextDiv(textContent: string, ...className: string[]): HTMLDivElement {// Funkcia na vytvorenie elementu div s danou triedou a textovým obsahom
+    const div = document.createElement("div");// Vytvorenie elementu div
+    div.classList.add(...className);// Pridanie triedy k elementu div
+    div.textContent = textContent;// Nastavenie textového obsahu elementu div
+    return div; // Vytvorenie elementu div s danou triedou a textovým obsahom
+}
+
+
+const lhRoh = vytvorTextDiv("", "roh", "horna", "lavy");
 sachovnica.append(lhRoh);
 
 for (let stlpec = 0; stlpec < 8; stlpec++) {
-    const hPopiska = document.createElement("div");
-    hPopiska.classList.add("popiska");
-    hPopiska.classList.add("horna");
-    hPopiska.textContent = String.fromCharCode(65 + stlpec);
+    const hPopiska = vytvorTextDiv(String.fromCharCode(65 + stlpec), "popiska", "horna");
     sachovnica.append(hPopiska);
 }
 
-const phRoh = document.createElement("div");
-phRoh.classList.add("roh");
-phRoh.classList.add("horna");
-phRoh.classList.add("prava");
+const phRoh = vytvorTextDiv("", "roh", "horna", "prava");
 sachovnica.append(phRoh);
 
+const vutro = vytvorTextDiv("", "vnutro");
+sachovnica.append(vutro);
+
 for (let riadok = 0; riadok < 8; riadok++) {
-    const lPopiska = document.createElement("div");
-    lPopiska.classList.add("popiska");
-    lPopiska.classList.add("lava");
-    lPopiska.textContent = `${8 - riadok}`;
+    const lPopiska = vytvorTextDiv(`${8 - riadok}`, "popiska", "lava");
     sachovnica.append(lPopiska);
 
     for (let stlpec = 0; stlpec < 8; stlpec++) {
-        const policko = document.createElement("div"); // Vytvorenie elementu div pre policko
-        policko.classList.add("policko"); // Pridanie triedy "policko" pre styling
+        const policko = vytvorTextDiv("", "policko"); // Vytvorenie elementu div pre policko
         if ((riadok + stlpec) % 2 === 0) {
             policko.classList.add("svetle"); // Pridanie triedy "svetle" pre svetlé policko
         } else {
             policko.classList.add("tmave");
         }
         //policko.textContent = String.fromCharCode(97 + stlpec) + `${8 - riadok}`;
-        sachovnica.append(policko); // Pridanie policka do kontajnera sachovnice
+        vutro.append(policko); // Pridanie policka do kontajnera sachovnice
     }
 
-    const pPopiska = document.createElement("div");
-    pPopiska.classList.add("popiska");
-    pPopiska.classList.add("prava");
+    const pPopiska = vytvorTextDiv(`${8 - riadok}`, "popiska", "prava");
     sachovnica.append(pPopiska);
-    pPopiska.textContent = `${8 - riadok}`;
 }
 
-const ldRoh = document.createElement("div");
-ldRoh.classList.add("roh");
-ldRoh.classList.add("dolna");
-ldRoh.classList.add("lavy");
+const ldRoh = vytvorTextDiv("", "roh", "dolna", "lavy");
 sachovnica.append(ldRoh); // Pridanie ľavého dolného rohu
 
 for (let stlpec = 0; stlpec < 8; stlpec++) {
-    const dPopiska = document.createElement("div");
-    dPopiska.classList.add("popiska");
-    dPopiska.classList.add("dolna");
-    dPopiska.textContent = String.fromCharCode(97 + stlpec);
+    const dPopiska = vytvorTextDiv(String.fromCharCode(97 + stlpec), "popiska", "dolna");
     sachovnica.append(dPopiska);
 }
 
-const pdRoh = document.createElement("div");
-pdRoh.classList.add("roh");
-pdRoh.classList.add("dolna");
-pdRoh.classList.add("prava");
+const pdRoh = vytvorTextDiv("", "roh", "dolna", "prava");
 sachovnica.append(pdRoh);
